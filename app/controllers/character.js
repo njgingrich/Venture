a271107b2517b47ccfd456d105988555ac2f4221
njgingrich <njgingrich@gmail.com>
2015-09-15 23:07:19 +0000

    added modifyStat function with lower-bound check

--- app/controllers/character.js
+++ app/controllers/character.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   character: Ember.computed.alias('model'),
 
+  _modifyStat: function(stat, amount) {
+    this.set('model.'+stat, this.get('model.'+stat)+amount);
+  },
 
   actions: {
     levelUp: function() {
@@ -10,10 +13,12 @@ export default Ember.Controller.extend({
     },
 
     increaseStat: function(stat) {
-      this.set('model.'+stat, this.get('model.'+stat)+1);
+      this._modifyStat(stat, 1);
     },
     decreaseStat: function(stat) {
-      this.set('model.'+stat, this.get('model.'+stat)-1);
+      if (this.get('model.'+stat) > 0) {
+        this._modifyStat(stat, -1);
+      }
     }
   }
 });
