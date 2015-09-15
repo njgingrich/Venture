079727ec34b0185518474670d96ad3d64bbd8d34
njgingrich <njgingrich@gmail.com>
2015-09-15 22:29:35 +0000

    added support for stat modification

--- app/controllers/character.js
+++ app/controllers/character.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  character: Ember.computed.alias('model'),
+
+
   actions: {
     levelUp: function() {
       this.set('model.level', this.get('model.level')+1);
+    },
+
+    increaseStat: function(stat) {
+      this.set('model.'+stat, this.get('model.'+stat)+1);
+    },
+    decreaseStat: function(stat) {
+      this.set('model.'+stat, this.get('model.'+stat)-1);
     }
   }
 });
--- app/models/character.js
+++ app/models/character.js
@@ -6,21 +6,29 @@ export default Ember.Object.extend({
   widsom: 2,
   dexterity: 8,
   constitution: 17,
-  health: Ember.computed('level', 'constitution', function() {
-    return this.get('constitution') * this.get('level');
-  }),
   charisma: 18,
+  health: Ember.computed('level', 'constitution', function() {
+    return 40 + (this.get('constitution') * this.get('level'));
+  }),
+  mana: Ember.computed('level', 'intelligence', function() {
+    return 30 + (this.get('intelligence') * this.get('level'));
+  }),
   level: 1,
 
   init() {
     this.set('name', generateName());
+    this.set('class', chooseClass());
   }
 });
 
 var generateName = function() {
   var firstNames = ['Ragnar', 'Ulfric', 'Balgruf'];
   var lastNames = ['Valiant', 'Bold', 'Strong', 'Dainty'];
-  var name = firstNames[Math.floor(Math.random()*firstNames.length)] + ' the ' +
-             lastNames[Math.floor(Math.random()*lastNames.length)];
-  return name;
+  return firstNames[Math.floor(Math.random()*firstNames.length)] + ' the ' +
+         lastNames[Math.floor(Math.random()*lastNames.length)];
 };
+
+var chooseClass = function() {
+  var classes = ['Wizard', 'Warrior', 'Bard', 'Paladin', 'Rogue', 'Ranger', 'Knight', 'Elf'];
+  return classes[Math.floor(Math.random()*classes.length)];
+}
