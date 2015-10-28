15c1d875f98b5a3d448cac68c32d786d41a75f94
njgingrich <njgingrich@gmail.com>
2015-10-28 23:24:05 +0000

    removed LocalStorage

--- app/transforms/item-bonuses.js
+++ app/transforms/item-bonuses.js
@@ -3,9 +3,11 @@ import Ember from 'ember';
 
 export default DS.Transform.extend({
     deserialize: function(serialized) {
+        console.log(serialized);
         return Ember.isNone(serialized) ? {} : serialized;
     },
     serialize: function(deserialized) {
+        console.log(deserialized);
         return Ember.isNone(deserialized) ? {} : deserialized;
     }
 });
