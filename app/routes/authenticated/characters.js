097e469423686de3a0cf3cd25f3e91f8539406c3
njgingrich <njgingrich@gmail.com>
2015-11-17 19:38:21 +0000

    added loading screen for characters

--- app/routes/authenticated/characters.js
+++ app/routes/authenticated/characters.js
@@ -1,7 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function() {
+    /*model: function() {
         return this.store.findAll('character');
-    }
+    }*/
+    model: function() {
+        let charPromise = this.store.findAll('character');
+        return new Ember.RSVP.Promise((resolve, reject) => {
+            return Ember.run.later(() => {
+                charPromise.then(chars => {
+                    if(chars.get('length') !== 0) {
+                        resolve(chars);
+                    } else {
+                        reject({error: "No chars found!"});
+                    }
+                });
+            }, 2000);
+        }).then(null, err => {alert(err.error)});
+    },
 });
