7bbb61de62c2af93288c27bc1123169ece7065cb
njgingrich <njgingrich@gmail.com>
2015-11-24 19:05:54 +0000

    added more map items for viewing

--- app/routes/authenticated/map.js
+++ app/routes/authenticated/map.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  map: Ember.inject.service(),
+  model: function() {
+    return this.store.findAll('character').then( chars => {
+      let char = chars.get('firstObject');
+      return this.store.query('world-view', { character_id: char.get('id')}).then((worldViews) => {
+        this.get('map').addToWorldView(worldViews.get('firstObject'));
+        this.get('map.allCells')[char.get('x')][char.get('y')].set('hasCharacter', true);
+        return char;
+      });
+    });
+  }
 });
--- app/routes/authenticated/play.js
+++ app/routes/authenticated/play.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function(params) {
-        var char = this.store.find('character', params.character);
-        return this.store.query('world-view', { 
-            character_id: char.get('id')}.then((worldViews) => {
-                
-        }));
+        return this.store.findRecord('character', params.character).then(char => {
+            return this.store.query('world-view', { character_id: char.get('id') }).then((worldViews) => {
+                var view = worldViews.get('firstObject');
+                return char;
+            });
+        });
     },
     serialize: function(model) {
         return { char: model.get('id') };
